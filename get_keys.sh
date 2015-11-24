8324411d8a7875f089dae83aa3427fb21ecf506d
Thiago Ribeiro <thiagitosouza@gmail.com>
2015-11-23 23:49:21 -0200

    Add script to get keys

---
+++ get_keys.sh
@@ -0,0 +1,8 @@
+echo "Hostname: $1"
+echo "Client: $2"
+echo "Destination: $3"
+scp root@$1:/etc/openvpn/easy-rsa/keys/$2.key $3
+scp root@$1:/etc/openvpn/easy-rsa/keys/$2.crt $3
+scp root@$1:/etc/openvpn/easy-rsa/keys/client.ovpn $3
+scp root@$1:/etc/openvpn/easy-rsa/keys/ca.crt $3
+
